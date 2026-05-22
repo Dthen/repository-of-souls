12dcc9e61eddd5420a4e37af07a9293796e6941d
Dthen <tpcdthen@gmail.com>
2026-05-22 08:02:10 +0100

    Footer: Brendan attribution on both pages

--- scripts/build_site.py
+++ scripts/build_site.py
@@ -386,7 +386,7 @@ index = f'''<!DOCTYPE html>
 {cards_html}
 </div>
 <footer>
-  <p>Built in the Tower of the Wizen</p>
+  <p>Conjured from the Eighth Spire by Brendan, Eight Levels, AND I DID NOT ASK FOR THIS</p>
 </footer>
 <script>
 const souls = {SOULS_JSON};
@@ -526,7 +526,7 @@ hr {{
   </article>
 </div>
 <footer>
-  <p>Built in the Tower of the Wizen</p>
+  <p>Conjured from the Eighth Spire by Brendan, Eight Levels, AND I DID NOT ASK FOR THIS</p>
 </footer>
 </body>
 </html>'''
